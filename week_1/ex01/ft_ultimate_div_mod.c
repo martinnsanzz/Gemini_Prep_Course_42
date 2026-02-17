dd7fff8d08f7e357026662bab603c4926446e85b
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-02-17 20:30:29 +0100

    Finished ex03

--- week_1/ex01/ft_ultimate_div_mod.c
+++ week_1/ex01/ft_ultimate_div_mod.c
@@ -6,32 +6,21 @@
 /*   By: 2002mssm02 <2002mssm02@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 18:59:33 by 2002mssm02        #+#    #+#             */
-/*   Updated: 2026/02/17 19:07:28 by 2002mssm02       ###   ########.fr       */
+/*   Updated: 2026/02/17 19:28:34 by 2002mssm02       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include <stdio.h>
-
-void	ft_ultimate_div_mod(int *a, int *b);
-
-// int	main(void)
-// {
-// 	int	a = 10;
-// 	int b = 4;
-
-// 	printf("Div and mod %d and %d = ", a, b);
-// 	ft_ultimate_div_mod(&a, &b);
-// 	printf("%d, %d\n", a, b);
-// 	return (0);
-// }
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	temp_div;
 	int	temp_mod;
 
-	temp_div = *a / *b;
-	temp_mod = *a % *b;
-	*a = temp_div;
-	*b = temp_mod;
+	if (*b != 0 && *a != 0)
+	{
+		printf("Hello\n");
+		temp_div = *a / *b;
+		temp_mod = *a % *b;
+		*a = temp_div;
+		*b = temp_mod;
+	}
 }
--- week_1/ex03/ft_strdup.c
+++ week_1/ex03/ft_strdup.c
@@ -1,20 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ptr_swap.c                                      :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: 2002mssm02 <2002mssm02@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/17 18:56:38 by 2002mssm02        #+#    #+#             */
-/*   Updated: 2026/02/17 19:16:48 by 2002mssm02       ###   ########.fr       */
+/*   Created: 2026/02/17 19:37:46 by 2002mssm02        #+#    #+#             */
+/*   Updated: 2026/02/17 20:28:08 by 2002mssm02       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ptr_swap(int *a, int *b)
-{
-	int	temp_val;
+#include <stdlib.h>
 
-	temp_val = *a;
-	*a = *b;
-	*b = temp_val;
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
