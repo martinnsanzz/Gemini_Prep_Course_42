dda80b13ce7dec2448316a175662bee45d57ab46
Martin Sanz Sanchez <martinuco569@gmail.com>
2026-02-21 11:35:51 +0100

    ex03 finished

---
+++ week_2/ex03/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 2002mssm02 <2002mssm02@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 11:29:17 by 2002mssm02        #+#    #+#             */
+/*   Updated: 2026/02/21 11:33:05 by 2002mssm02       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list *ft_list_last(t_list *begin_list)
+{
+	if (begin_list == NULL)
+		return (NULL);
+	while (begin_list->next != NULL)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
